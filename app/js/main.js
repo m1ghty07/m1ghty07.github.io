70230dff8bc053d915456417485347774bf5adbf
m1ghty07 <dulchenko_s@ukr.net>
2021-12-08 15:54:22 +0200

    add call btn, no scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,12 @@
 // Smooth scroll https://github.com/cferdinandi/smooth-scroll
-// new SmoothScroll('a[href*="#"]')
+const scroll = new SmoothScroll('a[href*="#"]');
+
+window.addEventListener("DOMContentLoaded", () => {
+  const call = document.querySelector(".call");
+
+  function callDisplay() {
+    call.style.right = "30px";
+  }
+
+  setInterval(callDisplay, 5000);
+});
