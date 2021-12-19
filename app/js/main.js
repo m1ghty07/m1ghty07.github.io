1804e798b0298fe396545159bdb4e01f86065054
m1ghty07 <dulchenko_s@ukr.net>
2021-12-19 17:50:42 +0200

    add scroll, php server

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", () => {
     call.style.right = "30px";
   }
 
-  setInterval(callDisplay, 5000);
+  setTimeout(callDisplay, 5000);
 
   const getResource = async (url) => {
     const res = await fetch(url);
@@ -36,18 +36,54 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  //  <div
-  //    class="portfolio__item"
-  //    style="background-image: url('./img/projects/oku.jpg');"
-  //  >
-  //    <div class="item__overlay-top">
-  //      <a href="">Github Pages</a>
-  //    </div>
-  //    <div class="item__title">
-  //      <span>Title</span>
-  //    </div>
-  //    <div class="item__overlay-bottom">
-  //      <a href="">Source code</a>
-  //    </div>
-  //  </div>;
+  function callByScroll() {
+    window.addEventListener("scroll", () => {
+      if (
+        window.pageYOffset + document.documentElement.clientHeight >=
+        document.documentElement.scrollHeight
+      ) {
+        call.style.right = "-300px";
+      } else {
+        call.style.right = "30px";
+      }
+    });
+  }
+  callByScroll();
+  // ===============================================================
+
+  let links = document.querySelectorAll('[href^="#"]'),
+    speed = 0.3;
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      let widthTop = document.documentElement.scrollTop,
+        hash = this.hash,
+        toBlock = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+
+      requestAnimationFrame(step);
+
+      function step(time) {
+        if (start === null) {
+          start = time;
+        }
+
+        let progress = time - start,
+          r =
+            toBlock < 0
+              ? Math.max(widthTop - progress / speed, widthTop + toBlock)
+              : Math.min(widthTop + progress / speed, widthTop + toBlock);
+
+        document.documentElement.scrollTo(0, r);
+
+        if (r != widthTop + toBlock) {
+          requestAnimationFrame(step);
+        } else {
+          location.hash = hash;
+        }
+      }
+    });
+  });
 });
